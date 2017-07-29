ed1d9cb40306cf2ceefe8d4f5284fe6beba15b51
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-07-29 22:29:50 +0200

    Code Jam 2017, tidy numbers, 15 points

---
+++ tidy_numbers.py
@@ -0,0 +1,16 @@
+def prv_tidy(value):
+    s = list(map(int, value))
+    for i in reversed(range(1, len(s))):
+        if s[i] < s[i-1]:
+            s[i-1] -= 1
+            s[i:] = [9 for x in s[i:]]
+
+    return int(''.join(str(x) for x in s))
+
+with open('input/B-large-practice.in') as f:
+    print(f.__next__())
+
+    i = 1
+    for line in f:
+        print('Case #{}: {}'.format(i, prv_tidy(line.strip())))
+        i += 1
