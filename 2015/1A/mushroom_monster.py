fdfc558b9e9c6709501b48f671d757a92e6f2f09
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-09-29 10:16:30 +0200

    Code Jam 2015, 1A, mushroom_monster, max score

--- 2015/1A/mushroom_monster.py
+++ 2015/1A/mushroom_monster.py
@@ -11,6 +11,8 @@ def evolution(snapshots):
     return [j-i for i, j in zip(snapshots[1:], snapshots[:-1])]
 
 
-val = list(map(int, '23 90 40 0 100 9'.split(' ')))
-print(mushroom_eating_method_1(val))
-print(mushroom_eating_method_2(val))
+with open('input/A-large-practice.in') as f:
+    lines = f.read().split('\n')
+    for i in range(int(lines[0])):
+        M = list(map(int, lines[2 * i + 2].split(' ')))
+        print('Case #{}: {} {}'.format(i + 1, mushroom_eating_method_1(M), mushroom_eating_method_2(M)))
